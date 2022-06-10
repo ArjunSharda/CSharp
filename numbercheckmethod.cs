d70896963b5190be96e2312681a4c8bd886dd09c
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-10 15:32:48 -0500

    Update numbercheckmethod.cs

--- numbercheckmethod.cs
+++ numbercheckmethod.cs
@@ -5,22 +5,21 @@ class Program
 {
     public static void Main (string[] args)
     {
-        numbercheck(500, 500);
+        numbercheck(502, 50);
     }
     static void numbercheck(int var1, int var2)
     {
-        if (var1 == var2)
-        {
-            Console.WriteLine("Both numbers are equal");
-        }
-        if (var1 > var2)
-        {
-            Console.WriteLine("Var1 is greater than var2.");
-        }
-        if (var2 > var1)
-        {
-            Console.WriteLine("Var2 is greater than var1.");
-        }
+      if (var1 == var2)
+      {
+        Console.WriteLine("Both numbers are equal");
+      }
+      if (var1 > var2)
+      {
+        Console.WriteLine("Var1 is greater than var2.");
+      }
+      if (var2 > var1)
+      {
+        Console.WriteLine("Var2 is greater than var1.");
+      }
     }
- 
 }
