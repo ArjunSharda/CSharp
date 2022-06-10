513a77d76805881c5f8468dc6e37b5dc2e9c78f1
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-10 13:51:09 -0500

    Create consolestringsaddwithspace.cs

---
+++ consolestringsaddwithspace.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string hello = "Hello";
+    string world = "world!";
+    Console.WriteLine(hello+" "+world);
+  }
+}
