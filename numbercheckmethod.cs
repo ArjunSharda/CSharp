b80195195ca0af2931963d22378dd44203ba0676
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-10 15:28:50 -0500

    Create numbercheckmethod.cs

---
+++ numbercheckmethod.cs
@@ -0,0 +1,26 @@
+using System;
+ 
+ 
+class Program
+{
+    public static void Main (string[] args)
+    {
+        numbercheck(500, 500);
+    }
+    static void numbercheck(int var1, int var2)
+    {
+        if (var1 == var2)
+        {
+            Console.WriteLine("Both numbers are equal");
+        }
+        if (var1 > var2)
+        {
+            Console.WriteLine("Var1 is greater than var2.");
+        }
+        if (var2 > var1)
+        {
+            Console.WriteLine("Var2 is greater than var1.");
+        }
+    }
+ 
+}
